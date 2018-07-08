813c2eacb0e3be6e2b5bdccc63eb601a13ea6fc4
Anai Araya <anai.yusary@gmail.com>
2018-07-08 23:13:17 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const parallax = document.getElementById('parallax')
+
+window.addEventListener('scroll', () => {
+    let offset = window.pageYOffset
+    //console.log('Offset: ' + offset)
+    //console.log('Offset: * 0.7' + offset * 0.7)
+    parallax.style.backgroundPositionY = offset * 0.5 + 'px'
+})
+
+
